5944d3dc153708e5c0bd537a4958dcf295f7ce48
patriciafaureforhat-lang <patriciafaureforhat@gmail.com>
2026-04-28 22:45:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ function setupSocialLinks() {
   if (instagramLink) {
     instagramLink.addEventListener("click", (e) => {
       e.preventDefault();
-      window.open("https://www.instagram.com/arteforhat", "_blank");
+      window.open("https://www.instagram.com/arte_forhat?igsh=MXhsazRhd3JkcTB5Mw==", "_blank");
     });
   }
 
@@ -164,7 +164,7 @@ function setupSocialLinks() {
   if (footerInstagram) {
     footerInstagram.addEventListener("click", (e) => {
       e.preventDefault();
-      window.open("https://www.instagram.com/arteforhat", "_blank");
+      window.open("https://www.instagram.com/arte_forhat?igsh=MXhsazRhd3JkcTB5Mw==", "_blank");
     });
   }
 
